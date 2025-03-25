f528884a013a56944a3a1ed01cfbb55e51209895
huangxin <axinnoemail@gmail.com>
2025-03-25 20:55:19 +0800

    语法分析器

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"Interpreter_In_Go/ast"
+	"Interpreter_In_Go/lexer"
+	"Interpreter_In_Go/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// 读取两个词法单元，已设置curtoken 和 peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
