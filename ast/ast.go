7314ed96c545d1e6cf5f0913aba00854b05fc8d2
huangxin <axinnoemail@gmail.com>
2025-03-25 20:48:24 +0800

    AST实现

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "Interpreter_In_Go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LaStatement struct {
+	Token token.Token // token.LET 词法单元
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LaStatement) statementNode()       {}
+func (ls *LaStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT 词法单元
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
