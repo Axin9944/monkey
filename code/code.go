94703a2d3cb0ce8a782344a3350f698c160a95a8
huangxin <axinnoemail@gmail.com>
2025-08-27 16:48:24 +0800

    编译器及虚拟机支持布尔类型

--- code/code.go
+++ code/code.go
@@ -22,6 +22,8 @@ const (
 	OpSub
 	OpMul
 	OpDiv
+	OpTrue
+	OpFalse
 )
 
 var definitions = map[Opcode]*Definition{
@@ -31,6 +33,8 @@ var definitions = map[Opcode]*Definition{
 	OpSub:      {"OpSub", []int{}},
 	OpMul:      {"OpMul", []int{}},
 	OpDiv:      {"OpDiv", []int{}},
+	OpTrue:     {"OpTrue", []int{}},
+	OpFalse:    {"OpFalse", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
